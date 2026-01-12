30610f48e743a835ad07f0ab87114285bf915f0b
Alexander Toftesund <alexandertoftesund@gmail.com>
2026-01-12 15:57:03 +0100

    initial react app

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <h1>Hello React!</h1>
+    <h1>Hello React!!</h1>
     <div>
       <h2>Jepp jepp</h2>
       <p>Some text!</p>
